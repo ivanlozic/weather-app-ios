d956fbccffff2e12a0866983926e1f0d8ea06c67
Ivan Lozic <ivanlozic995@gmail.com>
2025-04-05 14:35:48 +0200

    Initial Commit

---
+++ weather-app-kantunTests/weather_app_kantunTests.swift
@@ -0,0 +1,17 @@
+//
+//  weather_app_kantunTests.swift
+//  weather-app-kantunTests
+//
+//  Created by Ivan Lozic on 5.4.25..
+//
+
+import Testing
+@testable import weather_app_kantun
+
+struct weather_app_kantunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
